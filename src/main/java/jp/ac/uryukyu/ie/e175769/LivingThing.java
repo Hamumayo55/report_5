ce49431bbc1bf1b86d19681dabcce8fb1f8d2754
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2017-12-05 10:02:56 +0900

    Step1: コード準備

---
+++ src/main/java/jp/ac/uryukyu/ie/e175769/LivingThing.java
@@ -0,0 +1,82 @@
+package jp.ac.uryukyu.ie.e175769;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+    public LivingThing(String name,int hitPoint,int attack){
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+    }
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+
+    /**
+     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     * @return boolean
+     */
+    public boolean getDead(){
+        return  dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Heroへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
+    public void attack(LivingThing opponent){
+        if( getDead() == false ) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("\n", name);
+        }
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public void setAttack(int attack){
+
+        this.attack = attack;
+    }
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+}
